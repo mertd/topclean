3a60ac0c42ec8e51923eb624dde96be178c60b1f
Mert Demir <mertd@users.noreply.github.com>
2021-08-29 21:30:45 +0900

    Initial commit (#1)
    * initialize go mod
    
    * scoop
    
    * npm
    
    * use fmt and safer verify argument for npm
    
    * add yarn
    
    * add windows cleanmgr
    
    * remove aggressive switch
    
    * readme
    
    * rephrase
    
    * refactor catch/log

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # topclean
-Free up disk space with one command
+> Free up disk space with one command
+
+## Motivation and Inspiration
+
+`topclean` has been inspired by these projects:
+- [BleachBit](https://github.com/bleachbit/bleachbit)
+- [topgrade](https://github.com/r-darwish/topgrade)
+
+The goal is to provide disk cleaning functionality like BleachBit, but in a zero/low config way like topgrade offers for software upgrades. Another similarity to the latter is a focus on development tools.
+
+## Usage
+
+`topclean` is intended to be executed when you are running low on disk space and you are sure that deleting temporary files, caches, files marked for deletion (e.g. recycle bin) etc. is safe.
+
+Until builds are provided, you need to run `go run main.go`.
+
+## Licence
+
+[MIT](LICENCE.md)
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	fmt.Print("topclean\n\n")
+	apps := getApps()
+	for i := 0; i < len(apps); i++ {
+		app := apps[i]
+		err := clean(app)
+		catch(err)
+	}
+	fmt.Print("\ndone\n")
+}
+
+type App struct {
+	name string
+	cmd  string
+	args []string
+}
+
+func getApps() []App {
+	apps := []App{
+		{"scoop", "scoop", []string{"cleanup", "*"}},
+		{"npm", "npm", []string{"cache", "clean", "--force"}},
+		{"yarn", "yarn", []string{"cache", "clean"}},
+		{"cleanmgr", "cleanmgr", []string{"/d", "c", "/verylowdisk"}},
+	}
+	return apps
+}
+
+func clean(app App) error {
+	fmt.Println("Cleaning " + app.name)
+	var stdout, stderr bytes.Buffer
+	cmd := exec.Command(app.cmd, app.args...)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	catch(err)
+	cmdLog(stdout.String(), stderr.String())
+	return err
+}
+
+func cmdLog(stdout string, stderr string) {
+	if stdout != "" {
+		fmt.Println(stdout)
+	}
+	if stderr != "" {
+		fmt.Println(stderr)
+	}
+}
+
+func catch(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
