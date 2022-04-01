64649dc2397933ba561ae7920e3288a923425fe0
mertd <mertd@users.noreply.github.com>
2022-04-01 22:40:38 +0900

    skip

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,22 @@ fn s(input: &str) -> String {
 }
 
 struct App {
+    /** App Name */
     name: String,
+    /** Executable */
     cmd: String,
+    /** Arguments */
     args: Vec<String>,
+    /** Skip during test */
+    skip: bool,
 }
 
 impl App {
-    fn clean(&self) -> Output {
+    fn clean(&self) -> bool {
+        if self.skip {
+            println!("{} Skipping {}", PREFIX, self.name);
+            return false;
+        }
         println!("{} Cleaning {}", PREFIX, self.name);
         let mut command: Command;
         if cfg!(target_os = "windows") {
@@ -33,7 +42,7 @@ impl App {
             .expect(&[&self.name, "cleaning failed"].join(" "));
         io::stdout().write_all(&output.stdout).unwrap();
         io::stderr().write_all(&output.stderr).unwrap();
-        return output;
+        return true;
     }
 }
 
@@ -44,26 +53,31 @@ fn run() -> bool {
             name: s("scoop"),
             cmd: s("scoop"),
             args: vec![s("cleanup"), s("*")],
+            skip: false,
         },
         App {
             name: s("npm"),
             cmd: s("npm"),
             args: vec![s("cache"), s("clean"), s("--force")],
+            skip: false,
         },
         App {
             name: s("yarn"),
             cmd: s("yarn"),
             args: vec![s("cache"), s("clean")],
+            skip: false,
         },
         App {
             name: s("cleanmgr"),
             cmd: s("cleanmgr"),
             args: vec![s("/d"), s("c"), s("/verylowdisk")],
+            skip: true,
         },
         App {
             name: s("brew"),
             cmd: s("brew"),
             args: vec![s("cleanup")],
+            skip: false,
         },
     ];
     for app in apps {
